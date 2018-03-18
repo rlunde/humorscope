64d730f7111d899bb153422b38dbb46da398dada
Ron Lunde <rlunde@gmail.com>
2018-03-17 23:37:16 -0400

    add title and logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/newlogo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Daily Humorscope</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-date">
+          {new Date(Date.now()).toLocaleString()}
         </p>
       </div>
     );
