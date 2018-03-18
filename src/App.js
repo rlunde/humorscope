0ac466591aac7baf50f8b8f069bed6b18102d912
Ron Lunde <rlunde@gmail.com>
2018-03-18 00:22:46 -0400

    a little date formatting

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import logo from './images/newlogo.png';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    let now = new Date().toDateString()
+    let d = now.split(' ')
+    let datestr = `${d[0]}, ${d[1]} ${d[2]}, ${d[3]}`
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">The Daily Humorscope</h1>
+          <h4 className="App-date">
+          {datestr}
+        </h4>
         </header>
-        <p className="App-date">
-          {new Date(Date.now()).toLocaleString()}
-        </p>
+
       </div>
     );
   }
