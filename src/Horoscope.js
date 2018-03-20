cac76822223ccb4a5d1ca8b4bb101b483b58b6fa
Ron Lunde <rlunde@gmail.com>
2018-03-20 18:39:46 -0400

    more layout

--- src/Horoscope.js
+++ src/Horoscope.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Prediction from "./Prediction";
-let signs = ["Aries", "Taurus", "Gemini", "Cancer", 
-  "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", 
-  "Capricorn", "Aquarius", "Pisces"];
+
+let signs = [{ name: "Aries", dates: "Mar 21-Apr 19" },
+{ name: "Taurus", dates: "Apr 20-May 20" },
+{ name: "Gemini", dates: "May 21-Jun 20" },
+{ name: "Cancer", dates: "Jun 21-Jul 22" },
+{ name: "Leo", dates: "Jul 23-Aug 22" },
+{ name: "Virgo", dates: "Aug 23-Sep 22" },
+{ name: "Libra", dates: "Sep 23-Oct 22" },
+{ name: "Scorpio", dates: "Oct 23-Nov 21" },
+{ name: "Sagittarius", dates: "Nov 22-Dec 21" },
+{ name: "Capricorn", dates: "Dec 22-Jan 19" },
+{ name: "Aquarius", dates: "Jan 20-Feb 18" },
+{ name: "Pisces", dates: "Feb 19-Mar 20" }];
+
 let predictions = [];
 
 class Horoscope extends Component {
@@ -11,12 +22,13 @@ class Horoscope extends Component {
   render() {
     for (let s in signs) {
       // debugger;
-      predictions.push(<Prediction user="default" day="today" key={signs[s]} name={signs[s].toLowerCase()} />);
+      predictions.push(<Prediction user="default" day="today" key={signs[s].name}
+        name={signs[s].name} when={signs[s].dates} />);
     }
     return (
       <div className="Horoscope">
-        <table><tbody>
-            {predictions}
+        <table className="HoroscopeTable"><tbody>
+          {predictions}
         </tbody></table>
       </div>
     );
--- src/Prediction.js
+++ src/Prediction.js
@@ -3,11 +3,17 @@ import './App.css';
 
 function Prediction(props) {
     return (
-      <tr className="Prediction">
-                <td><div id={props.name} className="zodiacpic"/></td>
-                <td></td>
-      </tr>
+        <tr className="Prediction">
+            <td>
+                <div id={props.name.toLowerCase()} className="zodiacpic">                
+                </div>
+                <div className="sign">
+                {props.name}<br/>{props.when}
+                </div>
+            </td>
+            <td></td>
+        </tr>
     );
-  }
+}
 
 export default Prediction;
